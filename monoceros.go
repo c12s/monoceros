770a9cf8476869c8881789c3a1af963ade0c18dd
Tamara Ranković <rankovictamaraa@gmail.com>
2025-10-20 18:34:13 +0200

    .

--- monoceros.go
+++ monoceros.go
@@ -648,7 +648,9 @@ func (m *Monoceros) onAggregationReq(network *TreeOverlay, tree plumtree.TreeMet
 	// m.logger.Println("children to send req", receivers)
 	// m.logger.Println("has parent", network.plumtree.HasParent(tree.Id))
 	cancel := false
-	if (!m.HasHigherScore(tree.NodeID(), tree.Score)) {
+	if ((!m.HasHigherScore(tree.NodeID(), tree.Score) && tree.NodeID() != m.config.NodeID) || slices.ContainsFunc(network.activeRequests, func(r *ActiveAggregationReq) bool {
+		return r != nil && r.Tree.Score > tree.Score
+	})) && network.plumtree.HasParent(tree.Id) {
 		cancel = true
 	} else {
 		network.lastAggregationTime = int64(math.Max(float64(time.Now().UnixNano()), float64(network.lastAggregationTime)))
